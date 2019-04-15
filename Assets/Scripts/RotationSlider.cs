5401d07943f8d07e66ded00b5d6cd02397104301
Chavez <chavezs@SEAMC2000.local>
2019-04-15 13:22:54 -0700

    altered context. shadows and rescan still in progress

---
+++ Assets/Scripts/RotationSlider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotationSlider : MonoBehaviour
+{
+    public Slider slider;
+    public Vector3 vector;
+
+    private void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck();});
+    }
+
+    private void ValueChangeCheck()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(-slider.value, vector.y, vector.z);
+    }
+}
