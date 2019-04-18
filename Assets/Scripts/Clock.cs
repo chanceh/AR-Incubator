8e5811c63d968f6efddce8d26495edae9da33eb6
Chavez <chavezs@SEAMC2000.local>
2019-04-18 09:36:03 -0700

    fix clock, make new Main scene in Scenes/, add prefab with shadows and prefab without shadows

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Slider slider;
+
+    int hour;
+    int minutes;
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+    }
+
+    void Update()
+    {
+        hour = (int)(1+((slider.value / 15)+6)%12);
+        minutes = (int)(slider.value % 60);
+        Printable();
+    }
+
+    void Printable()
+    {
+        string StrHour = hour.ToString();
+        string StrMinutes = minutes.ToString("00");
+        text.text = StrHour + ":" + StrMinutes;
+    }
+}
